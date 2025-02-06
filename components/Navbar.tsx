b4c5e4370f333f7b781055a821581717bf523dd8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-06 21:53:07 +0300

    Mapping through the nav links and rendering them

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
-const Navbar = () => {
+export default function Navbar() {
+
+	const navLinks = ["Home", "About", "Blog", "For Business", "How-to Tips"];
+
 	return (
-		<div>Navbar</div>
+		<>
+		<nav>
+			<div>
+				<Link href="/">
+					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
+				</Link>
+
+				{navLinks.map((link) => (
+					<ul key={link}>
+						<li>
+							<Link href="/">
+								{link}
+							</Link>
+						</li>
+					</ul>
+				))}
+			</div>
+		</nav>
+</>
 	)
 }
-export default Navbar
+
