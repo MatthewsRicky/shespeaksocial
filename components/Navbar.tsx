0e1ce73c85fc0c2efaa885ef2ec194baa1b55535
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-06 22:13:17 +0300

    adding search input and call to action on the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,23 @@
+"use client"
+
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Navbar() {
 
-	const navLinks = ["Home", "About", "Blog", "For Business", "How-to Tips"];
+	const navLinks = ["Home", "About", "Blog", "For Business", "For Individuals", "Resources"];
 
 	return (
 		<>
 		<nav>
-			<div>
+			<div className="flex items-center justify-between mx-auto">
 				<Link href="/">
 					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
 				</Link>
 
 				{navLinks.map((link) => (
-					<ul key={link}>
+					<ul key={link} className="flex items-center justify-center mx-auto">
 						<li>
 							<Link href="/">
 								{link}
@@ -23,6 +25,12 @@ export default function Navbar() {
 						</li>
 					</ul>
 				))}
+
+				<div className="flex items-center justify-center mx-auto">
+					<input type="search" placeholder="Find Blog" />
+					<button type="submit">Get in Touch</button>
+				</div>
+
 			</div>
 		</nav>
 </>
