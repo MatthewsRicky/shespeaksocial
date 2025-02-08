7e8d4eff4ee311684bb14dadef893b27fcd44930
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-08 23:44:42 +0300

    adjusting more styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,8 +27,8 @@ export default function Navbar() {
 				))}
 
 				<div className="flex items-cente gap-2  justify-center mx-auto">
-					<input type="search" className="border-black border-2 border-solid" placeholder="Find Blog" />
-					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400 py-2 rounded-lg">Get in Touch</button>
+					<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
 				</div>
 
 			</div>
