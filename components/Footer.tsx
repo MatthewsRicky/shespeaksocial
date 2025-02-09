c347c20d905f259b035d1273ecc21bd16962315f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-09 23:52:51 +0300

    adding details to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,14 @@ const Footer = () => {
 		<section className="flex flex-col items-center justify-center mx-0 bg-slate-900 text-white w-full gap-6">
 			<div>
 				<div>
-					<div></div>
+					<div>
+						<h1>About Us</h1>
+						<article>We are known for our creativity, precision, and impact.
+							Our brand reflects our dedication to quality,
+							innovation, and social empowerment,
+							differentiating us in the market as not just service providers but as change-makers</article>
+						<h2>Follow Us</h2>
+					</div>
 					<div></div>
 					<div></div>
 				</div>
