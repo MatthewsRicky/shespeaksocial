bd6e38fc5f09ae5ddd7e255e0ec6840b01df6028
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-09 23:48:44 +0300

    copyright message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<section className="flex flex-col items-center justify-center mx-0 bg-slate-900">
+		<section className="flex flex-col items-center justify-center mx-0 bg-slate-900 text-white w-full gap-6">
 			<div>
 				<div>
 					<div></div>
 					<div></div>
 					<div></div>
 				</div>
-				<div></div>
+				<div>
+					<p>Copyright @ 2025 She Speaks Social Media Agency. All Rights Reserved </p>
+				</div>
 			</div>
 		</section>
 	)
