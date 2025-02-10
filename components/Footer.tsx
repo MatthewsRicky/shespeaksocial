410c6eb7203fff8df48ffb7dbaf3b7bd2e0147a5
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-10 09:55:13 +0300

    Filling out the 2nd and 3rd sections of the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,10 @@ import {FaInstagramSquare} from "react-icons/fa";
 
 const Footer = () => {
 	return (
-		<section className="flex flex-col items-center justify-center mx-0 bg-slate-900 text-white w-full gap-6">
+
+
+		<section
+			className="flex flex-col items-center justify-center mx-0 bg-slate-900 text-white w-full gap-6">
 			<div>
 				<div>
 					<div>
@@ -29,8 +32,24 @@ const Footer = () => {
 							</li>
 						</ul>
 					</div>
-					<div></div>
-					<div></div>
+					<div>
+						<h1>Explore</h1>
+						<article>1:1 Coaching
+							Templates Shop
+							Freelance Resources
+							Corporate Training & Coaching
+							Speaking & Events
+							Digital Literacy Programs
+							Freebies
+							News & Updates
+							Ebooks & Courses
+							Web Dev Services</article>
+					</div>
+					<div>
+						<h1>Newsletter</h1>
+						<article>Fill in your email address
+							to receive newsletters.</article>
+					</div>
 				</div>
 				<div>
 					<p>Copyright @ 2025 She Speaks Social Media Agency. All Rights Reserved </p>
