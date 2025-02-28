6cf4be2282cc7e74c41ba6b0da11bc9bf373d58d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-02-28 20:28:30 +0300

    mobile responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 	return (
 		<>
 		<nav>
-			<div className="flex items-center justify-between mx-auto">
+			<div className="hidden md:flex items-center justify-between mx-auto">
 				<Link href="/">
 					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
 				</Link>
@@ -31,6 +31,27 @@ export default function Navbar() {
 					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
 				</div>
 
+			</div>
+
+			<div className="flex flex-col md:hidden">
+				<Link href="/">
+					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
+				</Link>
+
+				{navLinks.map((link) => (
+					<ul key={link} className="flex items-center justify-between mx-auto gap-4">
+						<li className="flex items-center justify-around mx-auto px-3 py-2">
+							<Link href={"/" + link}>
+								{link}
+							</Link>
+						</li>
+					</ul>
+				))}
+
+				<div className="flex items-cente gap-2  justify-center mx-auto">
+					<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+				</div>
 			</div>
 		</nav>
 </>
