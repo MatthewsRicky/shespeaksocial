9989bf54a27bb40ca88f45217c3e6bd8295871c7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-01 22:44:20 +0300

    mobile responsive navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 	return (
 		<main>
 			<section>
-				<div>
+				<div style={{ backgroundImage: l('./home1-slider1.jpg') }} className="flex">
 					<h1 className="text-3xl">Home</h1>
 				</div>
 			</section>
