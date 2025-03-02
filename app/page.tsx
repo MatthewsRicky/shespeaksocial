7119711ef6473a58f7f495ab416f2c75d107ff5d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-02 14:33:40 +0300

    fixing image url

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 	return (
 		<main>
 			<section>
-				<div style={{ backgroundImage: l('./home1-slider1.jpg') }} className="flex justify-center items-center">
+				<div style={{ backgroundImage: ('./public/home1-slider1.jpg') }} className="flex justify-center items-center">
 					<h1 className="text-3xl">Home</h1>
 				</div>
 			</section>
