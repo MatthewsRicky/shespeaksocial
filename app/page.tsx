ffd1ae1c5d667bc0af1a724c3a280f28c1003cab
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-02 17:57:49 +0300

    width and height for the hero - still working on it

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@ import React from 'react'
 
 export default function Page() {
 	return (
-		<main>
-			<section>
-				<div style={{ backgroundImage: ('./public/home1-slider1.jpg') }} className="flex justify-center items-center">
-					<h1 className="text-3xl">Home</h1>
-				</div>
-			</section>
-		</main>
+<>
+	<section>
+		<div style={{
+			backgroundImage: ('./public/home1-slider1.jpg'),
+			width: "auto",
+			height: "auto"
+		}} className="flex justify-center items-center">
+			<h1 className="text-3xl">Home</h1>
+		</div>
+	</section>
+</>
+
 	)
 }
