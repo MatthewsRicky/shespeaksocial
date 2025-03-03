c9baef5a9ce66ee01bba1a84d2a8f7c8249f2de3
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-03 12:08:00 +0300

    More route def

---
+++ app/for-individuals/one-on-one-coaching/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const OneOnOne = () => {
+	return (
+		<div>OneOnOne</div>
+	)
+}
+export default OneOnOne
---
+++ app/for-individuals/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ForIndividuals = () => {
+	return (
+		<div>ForIndividuals</div>
+	)
+}
+export default ForIndividuals
