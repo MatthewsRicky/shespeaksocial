715ac34e63cda44638349f7169d6fc4b2156e11e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-04 18:33:08 +0300

    building out the service cards that follow the hero section

---
+++ components/ServiceCards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ServiceCards = () => {
+	return (
+		<div>
+			<header>
+				<h1>How can I help you</h1>
+				<div> </div>
+			</header>
+		</div>
+	)
+}
+export default ServiceCards
