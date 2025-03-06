4b2a2ab43ecdda7f9cf3af599c8cd7700603049f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-06 21:47:41 +0300

    Building out the card boiler plate

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from "next/image";
 
 const ServiceCards = () => {
 	return (
@@ -7,7 +8,13 @@ const ServiceCards = () => {
 				<h1>How can I help you</h1>
 				</header>
 				<div>
-					<div></div>
+					<div>
+						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
+						<div>
+							<h2>Make More Money</h2>
+							<p>We help free up your time so you can focus on what you do best - running your business.</p>
+						</div>
+					</div>
 					<div></div>
 					<div></div>
 					<div></div>
