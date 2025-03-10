12c5113354d2f000c89eb4ca8a073a28584836d1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-10 23:15:42 +0300

    more styling

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -7,23 +7,34 @@ const ServiceCards = () => {
 			<header>
 				<h1>How can I help you</h1>
 				</header>
-				<div className="flex items-center justify-between px-4 py-3 bg-pink-100  rounded-lg  m-8 text-slate-900">
-					<div className="flex sm:flex-col items-center justify-between  px-2 m-4 ">
+				<div className="flex sm:flex-row flex-col items-center justify-between px-4 py-3  rounded-lg  m-8 text-slate-900">
+					<div className="flex sm:flex-col items-center justify-between min-w-8  px-2 m-4 bg-pink-100">
+						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
+						<div className="flex-col gap-4 items-center justify-between rounded-lg py-4 px-2 ">
+							<h2>Make More Money</h2>
+							<p>We help free up your time so you can focus on what you do best - running your business.</p>
+						</div>
+					</div>
+					<div className="flex sm:flex-col items-center justify-between rounded-lg py-4 px-2 m-4 bg-pink-100">
 						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
 						<div className="flex-col gap-4 items-center justify-between px-2 ">
 							<h2>Make More Money</h2>
 							<p>We help free up your time so you can focus on what you do best - running your business.</p>
 						</div>
-					</div><div className="flex sm:flex-col items-center justify-between px-2 m-4 ">
+					</div>	<div className="flex sm:flex-col items-center justify-between rounded-lg py-4  px22 m-4 bg-pink-100">
 						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
 						<div className="flex-col gap-4 items-center justify-between px-2 ">
 							<h2>Make More Money</h2>
 							<p>We help free up your time so you can focus on what you do best - running your business.</p>
 						</div>
 					</div>
-
-
-
+					<div className="flex sm:flex-col items-center justify-between rounded-lg py-4 px-2 m-4 bg-pink-100">
+						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
+						<div className="flex-col gap-4 items-center justify-between px-2 ">
+							<h2>Make More Money</h2>
+							<p>We help free up your time so you can focus on what you do best - running your business.</p>
+						</div>
+					</div>
 				</div>
 
 		</div>
