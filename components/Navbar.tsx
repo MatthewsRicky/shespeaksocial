71faf7919fa5e53cfa96f9fbc878335bfb7d92db
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-17 21:23:22 +0300

    menu button styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,9 +13,10 @@ export default function Navbar() {
 
 	return (
 		<>
+
 		<nav className="flex inset-1 font-medium bg-gradient-to-r from-pink-200 to-red-200 h-96 items-center py-6 justify-center lg:h-32">
 
-			<button onClick={() => setMenuOpen(!menuOpen)}>
+			<button onClick={() => setMenuOpen(!menuOpen)} className="flex p-4 rounded-full m-4 bg-pink-500 shadow-emerald-950/60 shadow-md">
 				<span>
 					<BiMenu />
 				</span>
