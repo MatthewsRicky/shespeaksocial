bc1bcea2bf9cae3b1eca24aa1974f5703f01d5b2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-17 22:46:32 +0300

    getting closer to the final layout and conditional render

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,14 +14,17 @@ export default function Navbar() {
 	return (
 		<>
 
-		<nav className="flex inset-1 font-medium bg-gradient-to-r from-pink-100/90 to-green-100/60 h-96 items-center py-6 justify-center lg:h-32">
+		<nav className={`flex inset-1 font-medium bg-gradient-to-r from-pink-100/90 to-green-100/60 h-96 items-center py-6 justify-between lg:h-32 ${menuOpen}h-24`}>
+			{menuOpen && (
+				<Menu />
+			)}
 
-			<button onClick={() => setMenuOpen(!menuOpen)} className="flex p-4 rounded-full m-4 bg-pink-500 shadow-emerald-950/60 shadow-md">
+
+			<button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden flex p-4 rounded-full m-4 bg-pink-500 shadow-emerald-950/60 shadow-md">
 				<span>
 					<BiMenu />
 				</span>
 			</button>
-			<Menu />
 		</nav>
 </>
 	)
