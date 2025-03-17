22860109592f52e387182a8e85abd7c47043708e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-17 21:51:39 +0300

    moving the required variables to the Menu component

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+
+
 
 const Menu = () => {
+	const navLinks = [ "about", "blog", "payment", "for-business", "for Individuals", "resources"];
 	return (
-		<div>Menu</div>
+		<div>
+			<div className="hidden lg:flex items-center justify-between px-4 lg:w-[80%] mx-auto">
+				<Link href="/">
+					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
+				</Link>
+
+				{navLinks.map((link) => (
+					<ul key={link} className="flex items-center justify-around mx-auto gap-2">
+						<li className="flex items-center justify-around mx-auto px-2 py-2">
+							<Link href={"/" + link}>
+								{link}
+							</Link>
+						</li>
+					</ul>
+				))}
+
+				<div className="flex flex-col md:flex-row items-center gap-2  justify-center mx-auto">
+					<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+				</div>
+
+			</div>
+
+			<div className="flex flex-col lg:hidden">
+				<Link href="/">
+					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
+				</Link>
+
+				{navLinks.map((link) => (
+					<ul key={link} className="flex items-center justify-between mx-auto gap-4">
+						<li className="flex items-center justify-around mx-auto px-3 py-2">
+							<Link href={"/" + link}>
+								{link}
+							</Link>
+						</li>
+					</ul>
+				))}
+
+				<div className="flex flex-col items-cente gap-2 h-auto  justify-center mx-auto">
+					<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+				</div>
+			</div>
+		</div>
 	)
 }
 export default Menu
