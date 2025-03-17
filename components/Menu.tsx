d2a6478b95f641fbabac3120d1261d31d4dbeae1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-17 22:45:59 +0300

    conditionally styling the navbar based on the conditionally rendered state

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -30,26 +30,32 @@ const Menu = () => {
 
 			</div>
 
-			<div className="flex flex-col lg:hidden">
+			<div className="flex flex-col md:flex-row items-center gap-2  justify-center mx-auto">
+
 				<Link href="/">
-					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30} />
+					<Image src ="/shespeaksocial.png" alt="logo" width={144} height={30}  className="!flex"/>
 				</Link>
 
-				{navLinks.map((link) => (
-					<ul key={link} className="flex items-center justify-between mx-auto gap-4">
-						<li className="flex items-center justify-around mx-auto px-3 py-2">
-							<Link href={"/" + link}>
-								{link}
-							</Link>
-						</li>
-					</ul>
-				))}
+				<div className="flex flex-col lg:hidden">
 
-				<div className="flex flex-col items-cente gap-2 h-auto  justify-center mx-auto">
-					<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
-					<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+
+					{navLinks.map((link) => (
+						<ul key={link} className="flex items-center justify-between mx-auto gap-4">
+							<li className="flex items-center justify-around mx-auto px-3 py-2">
+								<Link href={"/" + link}>
+									{link}
+								</Link>
+							</li>
+						</ul>
+					))}
+
+					<div className="flex flex-col items-cente gap-2 h-auto  justify-center mx-auto">
+						<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+						<button type="submit" className="bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+					</div>
 				</div>
 			</div>
+
 		</div>
 	)
 }
