89554d48fe624c3c8e4e4f44def426471000d683
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-19 19:11:40 +0300

    Building out the component

--- components/HeroButton.tsx
+++ components/HeroButton.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const HeroButton = () => {
 	return (
-		<div>
+		<div className="flex">
 			<button className="flex -p-4 m-4 text-white bg-pink-400/80">request quote</button>
 		</div>
 	)
