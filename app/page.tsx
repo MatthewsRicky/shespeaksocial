374552d2e15a3958e14cefc4b6781fb6bcd2ed41
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-20 20:58:51 +0300

    Adding New Components to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import HeroSection from "@/components/HeroSection";
 import ServiceCards from "@/components/ServiceCards";
+import OptionsCarousel from "@/components/OptionsCarousel";
+import FounderAbout from "@/components/FounderAbout";
+import Reviews from "@/components/Reviews";
 
 const Home = () => {
 	return (
 		<div>
 			<HeroSection />
 			<ServiceCards />
+			<OptionsCarousel />
+			<FounderAbout />
+			<Reviews />
 		</div>
 	)
 }
