a5d7e395fc1e998319f58d5446a0b2fb4cd819dc
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-21 19:14:46 +0300

    Full Boiler-plate for the options Carousel

--- components/OptionsCarousel.tsx
+++ components/OptionsCarousel.tsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const OptionsCarousel = () => {
 	return (
-		<div>OptionsCarousel</div>
-	)
+		<>
+			<div>
+				<div>
+					<h1>Corporate Training & Coaching</h1>
+				</div>
+				<div>
+					<h1>Full Social Media Management</h1>
+				</div>
+				<div>
+					<h1>Speaking & Events</h1>
+				</div>
+				<div>
+					<h1>Digital Literacy Programs</h1>
+				</div>
+			</div>
+			<div>
+				<div>
+					<h1>1 : 1 Coaching</h1>
+				</div>
+				<div>
+					<h1>Templates Shop</h1>
+				</div>
+				<div>
+					<h1>Freelance Resources</h1>
+				</div>
+			</div>
+		</>
+			)
 }
 export default OptionsCarousel
