9ac95ef3037996c9eedf80ba359f8f0e2ec8b74a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-21 19:07:48 +0300

    grow your business card

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -31,10 +31,10 @@ const ServiceCards = () => {
 						</div>
 					</div>
 					<div className="flex sm:flex-col items-center justify-center text-center w-[90%] md:w-[23%] lg:w-[20%] rounded-lg min-h-80 py-4 px-2 m-2 sm:m-3 lg:m-4 bg-pink-100 md:translate-y-[-8] shadow-emerald-600/20 shadow-xl hover:scale-105">
-						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
+						<Image src="/grow.png" alt="Grow icon" width={50} height={50} />
 						<div className="flex-col gap-4 items-center justify-between py-3.5 px-2 ">
-							<h2 className="font-medium py-2">Make More Money</h2>
-							<p>We help free up your time so you can focus on what you do best - running your business.</p>
+							<h2 className="font-medium py-2">Grow Your Business On Social Media</h2>
+							<p>We help you identify your ideal audience, craft targeted content and ensure you&apos;re using the right platforms to achieve your goals.</p>
 						</div>
 					</div>
 				</div>
