f459a634703d55d84d4e8cb386363eac2664686c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-22 12:53:49 +0300

    review card component init

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ReviewCard from "@/components/ReviewCard";
 
 const Reviews = () => {
 	return (
-		<div>Reviews</div>
+		<section>
+			<div>
+				<h1>What our Customers say</h1>
+				<ReviewCard />
+			</div>
+		</section>
 	)
 }
 export default Reviews
