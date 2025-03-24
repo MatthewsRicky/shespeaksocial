c99c02f5ae4fa7687a7a04d82163feaa095b08eb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-24 12:20:59 +0300

    Functional previous and next buttons overlay on the hero

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,23 +2,27 @@
 
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import HeroButton from "@/components/HeroButton";
+import { BiChevronLeft,  BiChevronRight } from "react-icons/bi";
+
 
 const slides = [
 	{
 		image: "/home1-slider1.jpg",
 		title: "We Make People Care About you on Social Media",
 		subtitle: "We Offer A Comprehensive Suite Of Services Tailored To Meet the Unique Needs Of Entrepreneurs, Businesses, Social Media Managers And Freelancers.",
+		cta: "Start Exploring.",
 	},
 	{
 		image: "/home1-slider2.jpg",
 		title: "Double Your Social Media Engagement In Just One Month",
 		subtitle: "How would it feel to have a dedicated team backing your social media efforts? We are committed. ",
+		cta: "Join The Adventure.",
 	},
 	{
 		image: "/1729683914_site.jpg",
 		title: "Ready To Level Up Your Social Media?",
 		subtitle: "Get Your Free Consultation Today!",
+		cta: "Capture The Moment",
 	},
 ];
 
@@ -32,6 +36,9 @@ export default function HeroSection() {
 
 		return () => clearInterval(interval);
 	}, []);
+
+	const handleNext = () => setIndex((index) =>(index + 1) % slides.length);
+	const handlePrev = () => setIndex((index) =>(index - 1 + slides.length) % slides.length);
 
 	return (
 		<section className="relative h-[60vh] flex items-center justify-center text-white">
@@ -69,6 +76,22 @@ export default function HeroSection() {
 						<p className="text-lg mt-4">{slides[index].subtitle}</p>
 					</motion.div>
 				</AnimatePresence>
+			</div>
+
+			{/*{ Navigation Buttons}*/}
+			<div className="absolute inset-0 flex items-center justify-between px-4 z-10">
+				<button
+					onClick={handleNext}
+					className="p-2  bg-pink-500/60 hover:bg-pink-500/80 transition-all rounded-full"
+				>
+					<BiChevronLeft className="w-6 h-6" />
+				</button>
+				<button
+					onClick={handlePrev}
+					className="p-2 bg-pink-500/60 hover:bg-pink-500/80 transition-all rounded-full"
+				>
+					<BiChevronRight className="w-6 h-6" />
+				</button>
 			</div>
 		</section>
 	);
