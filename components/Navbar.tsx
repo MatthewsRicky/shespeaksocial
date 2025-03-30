855fc340376a067207f7001530b174d0b5185b16
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-30 20:40:38 +0300

    Fixed menu button affecting  large screen navbar seetings bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,37 +20,38 @@ export default function Navbar() {
 			<div className={`flex justify-around items-center w-full lg:w-[90%] mx-auto px-8`}>
 
 				<Logo />
+				<div className={`hidden lg:flex items-center justify-between px-4 lg:w-full mx-auto`}>
+
+					{navLinks.map((link) => (
+						<ul key={link} className="flex items-center justify-around mx-auto gap-2">
+							<li className="flex items-center justify-around mx-auto px-2 py-2">
+								<Link href={"/" + link}>
+									{link}
+								</Link>
+							</li>
+						</ul>
+					))}
+
+					<div className="flex flex-col md:flex-row items-center gap-2  justify-center mx-auto">
+						<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
+						<button type="submit" className=" flex flex-row justify-center items-center bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
+					</div>
+
+				</div>
 
 				{menuOpen && (
 					<div>
-						<div className={`hidden lg:flex items-center justify-between px-4 lg:w-full mx-auto`}>
 
-							{navLinks.map((link) => (
-								<ul key={link} className="flex items-center justify-around mx-auto gap-2">
-									<li className="flex items-center justify-around mx-auto px-2 py-2">
-										<Link href={"/" + link}>
-											{link}
-										</Link>
-									</li>
-								</ul>
-							))}
-
-							<div className="flex flex-col md:flex-row items-center gap-2  justify-center mx-auto">
-								<input type="search" className="rounded-lg shadow-black/30 shadow-lg border-black border-2 border-solid" placeholder="Find Blog" />
-								<button type="submit" className=" flex flex-row justify-center items-center bg-pink-500 px-3 shadow-lg shadow-pink-400/70 py-2 rounded-lg">Get in Touch</button>
-							</div>
-
-						</div>
 
 						<div className="flex flex-col md:flex-row items-center gap-2  justify-center mx-auto">
 
 
 
-							<div className="flex flex-col lg:hidden">
+							<div className="flex flex-col lg:hidden bg-pink-100/50 rounded w-full">
 
 
 								{navLinks.map((link) => (
-									<ul key={link} className="flex items-center justify-between mx-auto gap-6 bg-pink-100/50 rounded">
+									<ul key={link} className="flex items-center justify-between mx-auto gap-6 ">
 										<li className="flex items-center justify-around mx-auto px-3 py-2">
 											<Link href={"/" + link}>
 												{link}
@@ -70,7 +71,7 @@ export default function Navbar() {
 				)}
 
 
-				<button onClick={() => setMenuOpen(!menuOpen)} className="flex w-fit p-4 rounded-full m-4 bg-pink-500 shadow-emerald-950/60 shadow-md">
+				<button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden flex w-fit p-4 rounded-full m-4 bg-pink-500 shadow-emerald-950/60 shadow-md">
 				<span>
 					<BiMenu />
 				</span>
