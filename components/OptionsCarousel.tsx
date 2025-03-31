ef5b72830b2045836524b5a6fced2341460da0f8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-03-31 14:01:05 +0300

    setting up conditional renders

--- components/OptionsCarousel.tsx
+++ components/OptionsCarousel.tsx
@@ -1,33 +1,52 @@
-import React from 'react'
+"use client"
+
+import React, {useState} from 'react'
 
 const OptionsCarousel = () => {
+
+	const [optionOpen, setOptionOpen] = useState(true);
+	const [individualOpen, setIndividualOpen] = useState(true);
+
+
 	return (
 		<section className="flex gap-8 m-8 px-12 justify-start items-center ">
-			<div>
+
+			<button onClick={() => setOptionOpen(!optionOpen)}>For Businesses</button>
+			<button onClick={() => setIndividualOpen(!individualOpen)}>For Individuals</button>
+
+			{optionOpen ? (
 				<div>
-					<h1>Corporate Training & Coaching</h1>
+					<div>
+						<h1>Corporate Training & Coaching</h1>
+					</div>
+					<div>
+						<h1>Full Social Media Management</h1>
+					</div>
+					<div>
+						<h1>Speaking & Events</h1>
+					</div>
+					<div>
+						<h1>Digital Literacy Programs</h1>
+					</div>
 				</div>
+			) : 	!setOptionOpen}
+
+			{individualOpen ? (
 				<div>
-					<h1>Full Social Media Management</h1>
+					<div>
+						<h1>1 : 1 Coaching</h1>
+					</div>
+					<div>
+						<h1>Templates Shop</h1>
+					</div>
+					<div>
+						<h1>Freelance Resources</h1>
+					</div>
 				</div>
-				<div>
-					<h1>Speaking & Events</h1>
-				</div>
-				<div>
-					<h1>Digital Literacy Programs</h1>
-				</div>
-			</div>
-			<div>
-				<div>
-					<h1>1 : 1 Coaching</h1>
-				</div>
-				<div>
-					<h1>Templates Shop</h1>
-				</div>
-				<div>
-					<h1>Freelance Resources</h1>
-				</div>
-			</div>
+			) : !optionOpen}
+
+
+
 		</section>
 			)
 }
