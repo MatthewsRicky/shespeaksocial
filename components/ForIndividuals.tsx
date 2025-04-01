7505d5bfcc52502741801c5aa80ebf7542d51119
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-01 14:52:06 +0300

    for individuals init

---
+++ components/ForIndividuals.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ForIndividuals = () => {
+	return (
+		<div>
+			<div>
+				<h1>1 : 1 Coaching</h1>
+			</div>
+			<div>
+				<h1>Templates Shop</h1>
+			</div>
+			<div>
+				<h1>Freelance Resources</h1>
+			</div>
+		</div>
+	)
+}
+export default ForIndividuals
