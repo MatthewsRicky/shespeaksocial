236c6ab682226f149a52d417be25ece60e4374f0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-01 14:51:35 +0300

    for business int

---
+++ components/ForBusiness.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ForBusiness = () => {
+	return (
+		<div className="w-screen ">
+			<div>
+				<h1>Corporate Training & Coaching</h1>
+			</div>
+			<div>
+				<h1>Full Social Media Management</h1>
+			</div>
+			<div>
+				<h1>Speaking & Events</h1>
+			</div>
+			<div>
+				<h1>Digital Literacy Programs</h1>
+			</div>
+		</div>
+	)
+}
+export default ForBusiness
