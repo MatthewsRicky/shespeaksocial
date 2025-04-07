ce49c399cf8698bc7d4c2e4d19097365509013ae
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-07 22:59:11 +0300

    coming together nicely

--- components/ForBusiness.tsx
+++ components/ForBusiness.tsx
@@ -9,25 +9,29 @@ const ForBusiness = () => {
 		description: "Equip your workforce with the essential digital skills needed to thrive in today's online world",
 		button: "Get started",
 		id: 1,
-	},{
+	},
+	{
 		imageUrl: "/social-media-mgt.png",
 		title: "Full Social Media Management",
 		description: "For dedicated and ambitious entrepreneurs looking to hand over their social media to an expert team.",
 		button: "Shop Now",
 		id: 2,
-	},{
+	},
+	{
 		imageUrl: "/team_engage.jpg",
 		title: "Speaking & Events",
 		description: "Elevate your events with engaging and insightful talks from our seasoned social media experts.",
 		button: "Get in touch",
 		id: 3,
-	},{
+	},
+	{
 		imageUrl: "/corporate_engage.jpg",
 		title: "Digital Literacy Programs",
 		description: "Designed to equip underprivileged youth with the skills to succeed in the digital world.",
 		button: "Sign up",
 		id: 4,
-	},]
+	},
+]
 	return (
 		<div className="w-screen ">
 			{businessCard.map((item) => (
