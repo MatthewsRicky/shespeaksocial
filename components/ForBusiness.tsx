632df3087a8f37d5c0a217cc15ba4e003066400a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-07 16:35:40 +0300

    Building out the cards component for all the biz options database

--- components/ForBusiness.tsx
+++ components/ForBusiness.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
 
 const ForBusiness = () => {
+
+	const businessCard = [{
+		imageUrl: (""),
+		title: "Corporate Training & Coaching",
+		description: "Equip your workforce with the essential digital skills needed to thrive in today's online world",
+		button: "Get started",
+	},{
+		imageUrl: (""),
+		title: "Full Social Media Management",
+		description: "For dedicated and ambitious entrepreneurs looking to hand over their social media to an expert team.",
+		button: "Shop Now",
+	},{
+		imageUrl: (""),
+		title: "Speaking & Events",
+		description: "Elevate your events with engaging and insightful talks from our seasoned social media experts.",
+		button: "Get in touch",
+	},{
+		imageUrl: (""),
+		title: "Digital Literacy Programs",
+		description: "Designed to equip underprivileged youth with the skills to succeed in the digital world.",
+		button: "Sign up",
+	},]
 	return (
 		<div className="w-screen ">
 			<div>
