8382c672b33ccce9ca07d33a2bce5b428a4724c4
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-07 20:06:11 +0300

    Key and id set up for map() functionality

--- components/ForBusiness.tsx
+++ components/ForBusiness.tsx
@@ -1,42 +1,41 @@
 import React from 'react'
+import Image from "next/image";
 
 const ForBusiness = () => {
 
 	const businessCard = [{
-		imageUrl: (""),
+		imageUrl: "/consulting.jpg",
 		title: "Corporate Training & Coaching",
 		description: "Equip your workforce with the essential digital skills needed to thrive in today's online world",
 		button: "Get started",
+		id: 1,
 	},{
-		imageUrl: (""),
+		imageUrl: "",
 		title: "Full Social Media Management",
 		description: "For dedicated and ambitious entrepreneurs looking to hand over their social media to an expert team.",
 		button: "Shop Now",
+		id: 2,
 	},{
-		imageUrl: (""),
+		imageUrl: "",
 		title: "Speaking & Events",
 		description: "Elevate your events with engaging and insightful talks from our seasoned social media experts.",
 		button: "Get in touch",
+		id: 3,
 	},{
-		imageUrl: (""),
+		imageUrl: "",
 		title: "Digital Literacy Programs",
 		description: "Designed to equip underprivileged youth with the skills to succeed in the digital world.",
 		button: "Sign up",
+		id: 4,
 	},]
 	return (
 		<div className="w-screen ">
-			<div>
-				<h1>Corporate Training & Coaching</h1>
-			</div>
-			<div>
-				<h1>Full Social Media Management</h1>
-			</div>
-			<div>
-				<h1>Speaking & Events</h1>
-			</div>
-			<div>
-				<h1>Digital Literacy Programs</h1>
-			</div>
+			{businessCard.map((item) => (
+				<div key={item.id}>
+					<Image src={item.imageUrl} alt="Consulting" width={400} height={400} />
+					<h1>{item.title}</h1>
+				</div>
+			))}
 		</div>
 	)
 }
