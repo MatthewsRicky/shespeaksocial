857016633a1529243a01747fc7557965d3a3d3a9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-12 22:29:55 +0300

    creating all the objects

--- components/ForIndividuals.tsx
+++ components/ForIndividuals.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
 
 const ForIndividuals = () => {
+
+	const individual = [
+		{
+			id : 1,
+			imgurl: "",
+			title: "",
+			description:"",
+			btnlabel:"",
+		},
+		{
+			id : 1,
+			imgurl: "",
+			title: "",
+			description:"",
+			btnlabel:"",
+		},
+		{
+			id : 1,
+			imgurl: "",
+			title: "",
+			description:"",
+			btnlabel:"",
+		},
+	]
+
 	return (
 		<div className="flex justify-center items-center gap-8 m-4 px-3 py-4">
 			<div>
