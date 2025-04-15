914af3e6e66a346d07d87c3283b6d96169c739d2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-15 11:38:44 +0300

    mapping through and adding the images

--- components/ForIndividuals.tsx
+++ components/ForIndividuals.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from "next/image";
 
 const ForIndividuals = () => {
 
@@ -9,6 +10,7 @@ const ForIndividuals = () => {
 			title: "1:1 Coaching",
 			description:"Social Strategy Coaching for freelancers, marketers and entrepreneurs to help you reach long-term success.",
 			btnlabel:"Let's Chat",
+			alt: "Coaching",
 		},
 		{
 			id : 2,
@@ -16,6 +18,7 @@ const ForIndividuals = () => {
 			title: "Templates Shop",
 			description:"Browse in demand templates, designed to help you get organized, save time and grow your socials.",
 			btnlabel:"Shop now",
+			alt: "Templates",
 		},
 		{
 			id : 3,
@@ -23,23 +26,17 @@ const ForIndividuals = () => {
 			title: "Freelance Resources",
 			description:"Turn your social media passion into a fully fledged career with these MUST-HAVE freelance tools and kits.",
 			btnlabel:"Learn more",
+			alt: "Freelance",
 		},
 	]
 
 	return (
 		<div className="flex justify-center items-center gap-8 m-4 px-3 py-4">
-			<div>
-				<h1 className=
-				'flex justify-center items-center mx-auto text-lg font-bold'>1 : 1 Coaching</h1>
-			</div>
-			<div>
-				<h1 className=
-				'flex justify-center items-center mx-auto text-lg font-bold'>Templates Shop</h1>
-			</div>
-			<div>
-				<h1 className=
-				'flex justify-center items-center mx-auto text-lg font-bold'>Freelance Resources</h1>
-			</div>
+			{individual.map((item, index) => (
+				<div key={item.id}>
+					<Image src={item.imgurl} alt={item.alt} />
+				</div>
+			))}
 		</div>
 	)
 }
