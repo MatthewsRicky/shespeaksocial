6c1d352e1174fe99936600b1ff24b3c52d56bd5c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-16 20:23:00 +0300

    image styling

--- components/ForIndividuals.tsx
+++ components/ForIndividuals.tsx
@@ -33,9 +33,9 @@ const ForIndividuals = () => {
 	return (
 		<div className="flex justify-center items-center shadow-lg rounded-lg shadow-violet-300 gap-8 m-4 px-3 py-4">
 			{individual.map((item) => (
-				<div key={item.id} className="flex flex-col items-center justify-center py-2 shadow-md shadow-pink-200 gap-4">
-					<Image src={item.imgurl} alt={item.alt} width={500} height={500} />
-					<h1>{item.title}</h1>
+				<div key={item.id} className="flex flex-col items-center justify-center p-2 shadow-md shadow-pink-200 gap-y-4">
+					<Image src={item.imgurl} alt={item.alt} width={500} height={500} className="rounded-md flex" />
+					<h1 >{item.title}</h1>
 					<p>{item.description}</p>
 					<button>{item.btnlabel}</button>
 				</div>
