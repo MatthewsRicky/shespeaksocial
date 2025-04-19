7d41d9c50807ecfa423aec414e4cce6d81efdd91
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-19 17:06:11 +0300

    Jeff Mkungusi

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -8,12 +8,12 @@ const reviews = [
 		description: "Working with Shirlyn and her team is like winning the lotto. I couldn't have dreamed up a better person to collaborate with.  You just don't find people who are as talented, professional, driven, communicative, kind and fun as she is.  Thanks to Shirlyn my work feels like my ultimate purpose is being fulfilled while I'm having the party of my life. It's a blessing to know her and an honor to create and work with her.",
 	},
 	{
-		imgurl: "",
-		name: "",
-		description: "",
+		imgurl: "about2.jpeg",
+		name: "Jeff Mkungusi",
+		description: "Working with She Speaks Social transformed my business. Their digital marketing strategy doubled my social media engagement and website traffic, bringing in not just more leads but the right customers. The team was proactive, communicative, and genuinely invested in my success. They’re more than a service provider—they’re a true partner. Highly recommend!",
 	},
 	{
-		imgurl: "",
+		imgurl: "about2.jpeg",
 		name: "",
 		description: "",
 	},
