05196db6a020cc98837bdf6cd7a4f9d2d53083a6
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-19 17:05:12 +0300

    Kim .LaPaglia

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ReviewCard = () => {
 const reviews = [
 	{
-		imgurl: "",
+		imgurl: "about2.jpeg",
 		name: "Kim LaPaglia",
 		description: "Working with Shirlyn and her team is like winning the lotto. I couldn't have dreamed up a better person to collaborate with.  You just don't find people who are as talented, professional, driven, communicative, kind and fun as she is.  Thanks to Shirlyn my work feels like my ultimate purpose is being fulfilled while I'm having the party of my life. It's a blessing to know her and an honor to create and work with her.",
 	},
