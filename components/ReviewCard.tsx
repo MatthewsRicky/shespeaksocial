10211afa002964f390907135e88f776287040fc1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-19 17:02:47 +0300

    Building out the review card.

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const ReviewCard = () => {
-
+const reviews = [
+	{
+		imgurl: "",
+		name: "Kim LaPaglia",
+		description: "Working with Shirlyn and her team is like winning the lotto. I couldn't have dreamed up a better person to collaborate with.  You just don't find people who are as talented, professional, driven, communicative, kind and fun as she is.  Thanks to Shirlyn my work feels like my ultimate purpose is being fulfilled while I'm having the party of my life. It's a blessing to know her and an honor to create and work with her.",
+	},
+	{
+		imgurl: "",
+		name: "",
+		description: "",
+	},
+	{
+		imgurl: "",
+		name: "",
+		description: "",
+	},
+]
 
 	return (
 		<div>ReviewCard</div>
