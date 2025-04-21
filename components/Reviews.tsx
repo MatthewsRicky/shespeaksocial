d5cb86b92e5b59c8426c0a58f02f38a6518cd75d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-21 21:07:08 +0300

    Title font-color

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -5,7 +5,7 @@ const Reviews = () => {
 	return (
 		<section>
 			<div className="flex flex-col gap-6 items-center justify-between mt-12">
-				<h1>What our Customers say</h1>
+				<h1 className="flex item-center justify-center mx-auto font-bold text-2xl text-slate-800 underline underline-offset-20 text-shadow-slate-600/40 text-shadow-lg">What our Customers say</h1>
 				<ReviewCard />
 			</div>
 		</section>
