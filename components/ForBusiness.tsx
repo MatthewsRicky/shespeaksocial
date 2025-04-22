89724fc17e30c645b838ef732c42c4205103b83a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-22 22:38:52 +0300

    style edit

--- components/ForBusiness.tsx
+++ components/ForBusiness.tsx
@@ -37,10 +37,10 @@ const ForBusiness = () => {
 	},
 ]
 	return (
-		<div className="h-240 m-4 w-[100%] grid grid-cols-1 md:grid-cols-2 md:w-full justify-between items-center rounded-lg gap-12">
+		<div className="h-240 m-4 w-full grid grid-cols-1 md:grid-cols-2 md:w-[80%] justify-between items-center rounded-lg gap-12">
 			{businessCard.map((item) => (
-				<div key={item.id} className="flex rounded-xl min-h-100 text-center shadow-violet-400/50 shadow-lg gap-6 flex-col justify-center items-center overflow-x-0 bg-linear-to-b from-pink-50 to-pink-100 h-full w-full">
-					<Image src={item.imageUrl} alt={item.alt} width={400} height={400} className="flex cover shadow-lg hover:scale-105 transition-all duration-200 shadow-violet-400/30"/>
+				<div key={item.id} className="flex rounded-xl lg:h-full text-center shadow-violet-400/50 shadow-lg gap-6 flex-col justify-center items-center overflow-none bg-linear-to-b from-pink-50 to-pink-100 h-full w-[80%] mx-auto">
+					<Image src={item.imageUrl} alt={item.alt} width={400} height={400} className="flex cover shadow-lg w-full hover:scale-105 transition-all duration-200 shadow-violet-400/30"/>
 					<h1 className="text-center font-medium uppercase text-sm p-1 shadow-sm mx-3 rounded-md text-slate-900 shadow-violet-300">{item.title}</h1>
 					<p className="font-normal text-center p-2 mx-2 text-sm">{item.description}</p>
 					<button onClick={() => {}} type={"button"} className="border my-2 px-3 py-2 rounded-sm bg-pink-400/90 text-slate-800 hover:scale-105 hover:bg-pink-500 hover:text-slate-200 transition-all duration-200 hover:shadow-pink-500 hover:shadow-sm hover:border-pink-500 border-pink-300 text-center">{item.button}</button>
