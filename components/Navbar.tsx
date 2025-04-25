cf8532539eca11f6f404f441cf591afcc23fcba6
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-25 19:25:41 +0300

    sorting the final routing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
 
 	const [menuOpen, setMenuOpen] = useState(false);
-	const navLinks = [ "about", "blog", "payment", "for-business", "for Individuals", "resources"];
+	const navLinks = [ "about", "blog", "payment", "for-business", "for-individuals", "resources"];
 	return (
 		<>
 
