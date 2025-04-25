b0ede472183e8dc66cdf79bd8c385d6ae1aa02bd
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-25 19:24:22 +0300

    sorting the route and adding initial component

--- app/for-business/page.tsx
+++ app/for-business/page.tsx
@@ -1,8 +1,11 @@
+"use client"
 import React from 'react'
-
-const ForBusiness = () => {
+import  ForBusiness  from "@/components/ForBusiness"
+const Business = () => {
 	return (
-		<div>For Business</div>
+		<div>
+			<ForBusiness />
+		</div>
 	)
 }
-export default ForBusiness
+export default Business
