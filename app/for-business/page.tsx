d2bcf9e73a36d1dcce215d789590cd7d3fc81f6a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-27 13:10:50 +0300

    business route page initial layout

--- app/for-business/page.tsx
+++ app/for-business/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import  ForBusiness  from "@/components/ForBusiness"
 const Business = () => {
 	return (
-		<div>
+		<div className="flex flex-col items-center">
 			<ForBusiness />
 		</div>
 	)
