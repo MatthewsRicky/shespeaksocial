52868b42e659a7bf03d25e3e30871821f1a8a2f2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-04-27 18:51:31 +0300

    building out the blog object array structure for the database. Hard coded for now*

---
+++ components/Blog.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Blog = () => {
+
+	const posts = [
+		{
+			imageUrl: "/career.jpg",
+			imageAlt: "Career",
+			imageTitle: "careers",
+			title: "Top Remote Jobs Hiring Now: High-Paying Digital Careers You Can Apply for Today",
+			article: "There was a time when “work” meant fluorescent-lit offices, morning commutes in bumper-to-bumper traffic, and counting the hours until Friday.…",
+			date: "March 1, 2025",
+			author: "She Speaks Social",
+		},
+	]
+
+	return (
+		<div>Blog</div>
+	)
+}
+export default Blog
