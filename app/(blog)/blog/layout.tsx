aea060cf01f006898c2e1efcef3a7a75c752d865
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-06 11:37:40 +0300

    Blog layout file

---
+++ app/(blog)/blog/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../../globals.css";
+
+import React from "react";
+import BlogNavbar from "@/components/BlogNavbar";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Blog",
+	description: "This blog is attached to the shespeaksocial.com website." +
+		" Educational services available as well as custom made brand campaigns,"
+};
+
+export default function RootLayout({
+	                                   children,
+                                   }: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<div>
+					<BlogNavbar />
+					{children}
+				</div>
+			</body>
+		</html>
+	);
+}
