10715c1b68e1314bc85aeb8ce8d806ef28c09283
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-06 11:35:50 +0300

    route grouping so I can have different layouts

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import React from "react";
@@ -31,9 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Navbar />
-        {children}
-      <Footer />
+      <main>
+	      <Navbar />
+	      {children}
+	      <Footer />
+      </main>
       </body>
     </html>
   );
