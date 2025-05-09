13282e97b9e54827b682883046361810d8835cee
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-09 20:20:23 +0300

    styling the main container layout

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -3,7 +3,7 @@ import Blog from "@/components/Blog";
 
 const About = () => {
 	return (
-		<div>
+		<div className="flex items-center justify-center w-full lg:w-95vw xl:w-90vw mx-auto">
 			<Blog />
 		</div>
 	)
