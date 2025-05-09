20523a8e58f5469b51441aa386eebe765a78fd05
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-09 22:40:10 +0300

    Layout resizing of the cards

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 
 const Blog = () => {
 	return (
-		<main className="flex w-full items-center my-48 justify-center mx-auto">
+		<main className="flex flex-col lg:flex-row w-full items-center justify-center mx-auto mt-48 lg:mt-16">
 			<section className="grid grid-cols-1 md:grid-cols-2 justify-center items-center h-[40vh] gap-6">
 				{posts.map((post) => (
-					<div key={post.id} className="relative flex flex-col items-center justify-center w-full gap-2">
-						<Image src={post.imageUrl} alt={post.imageAlt} width={500} height={200} className="flex"/>
+					<div key={post.id} className="relative flex flex-col items-center justify-center w-full gap-2 h-96">
+						<Image src={post.imageUrl} alt={post.imageAlt} width={200} height={200} className="flex"/>
 						<span>{post.imageTitle}</span>
 						<h1>{post.title}</h1>
 						<span>
