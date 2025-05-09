d4e9e1a58ba39e6b3066c0760bb8a430b723c4ed
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-09 22:44:36 +0300

    building out the blo sidebar container

--- components/BlogSideBar.tsx
+++ components/BlogSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogSideBar = () => {
 	return (
-		<div className="hidden lg:flex w-[30vw] h-screen">BlogSideBar</div>
+		<div className="hidden h-fit  lg:flex items-center justify-center mx-auto bg-slate-700  w-full text-white">BlogSideBar</div>
 	)
 }
 export default BlogSideBar
