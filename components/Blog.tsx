f685ae3d3b5dee8083ffdd7a63255ec9297e1606
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-12 20:04:56 +0300

    fixing the layout

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 
 const Blog = () => {
 	return (
-		<main className="grid grid-cols-1 md:grid-cols-2 w-full items-center justify-start mx-auto overflow-x-hidden">
-			<section className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-6">
+		<main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full items-center justify-center mx-auto overflow-x-hidden lg:mt-48">
+			<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-6">
 				{posts.map((post) => (
 					<div key={post.id} className="relative flex flex-col items-center justify-center w-full gap-2 h-96">
 						<Image src={post.imageUrl} alt={post.imageAlt} width={200} height={200} className="flex"/>
@@ -21,11 +21,11 @@ const Blog = () => {
 					</div>
 
 				))}
-
+				<div className="hidden w-full h-fit lg:flex">
+					<BlogSideBar />
+				</div>
 			</section>
-			<div className="hidden w-full  lg:flex">
-				<BlogSideBar />
-			</div>
+
 
 		</main>
 	)
