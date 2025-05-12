56be594c48749605509541625c441c57dfa75d2e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-12 20:05:37 +0300

    Blog sidebar design

--- components/BlogSideBar.tsx
+++ components/BlogSideBar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const BlogSideBar = () => {
 	return (
-		<div className="hidden h-full lg:flex flex-col items-center justify-center mx-auto bg-slate-700  w-full text-white">
+		<div className="max-h-fit flex flex-col items-center justify-center mx-auto bg-slate-700  w-full text-white">
 				{posts.map((post) => (
 					<section key={post.id} className="flex flex-col items-center justify-center W-full">
 						<Image src={post.imageUrl} alt={post.imageAlt} width={400} height={400} />
