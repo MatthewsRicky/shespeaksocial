63a6f22504fc6f18a88978116b82efb5fd3a7e7a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-13 18:09:32 +0300

    factoring the service cards into an array for mapping through

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -3,40 +3,53 @@ import Image from "next/image";
 
 
 const ServiceCards = () => {
+
+	const service =[
+		{
+			id: 1,
+			imgUrl: "/make-money.png",
+			title: "Make More Money",
+			description: "We help free up your time so you can focus on what you do best - running your business.",
+		},
+		{
+			id: 2,
+			imgUrl: "/like.png",
+			title: "Turn Likes into Clients",
+			description: "We help you convert your online audience into loyal customers and boost your bottom line.",
+		},
+		{
+			id: 3,
+			imgUrl: "/social.png",
+			title: "Level Your Social Media Skills",
+			description: "Take advantage of our free ebooks, training programs and workshops to stay ahead of the curve",
+		},
+		{
+			id: 4,
+			imgUrl: "/grow.png",
+			title: "Grow Your Business On Social Media",
+			description: "We help you identify your ideal audience, craft targeted content and ensure you&apos;re using the right platforms to achieve your goals.",
+		},
+	]
+
 	return (
 		<div className="flex items-center justify-center scroll-auto max-w-fit h-fit bg-linear-to-br flex-col py-10">
 			<header>
 				<h1 className="font-bold text-3xl">How can I help you</h1>
 				</header>
 				<div className="flex flex-col md:flex-row items-center justify-between px-4 py-3 m-w-[90%] lg:w-[80%] mx-auto rounded-lg  m-4 text-slate-900">
-					<div className="flex sm:flex-col items-center justify-center text-center h-[23%] w-[90%] md:w-[23%] lg:w-[20%] rounded-lg  min-h-[28rem] py-4 px-2 m-2 sm:m-3 lg:m-4 bg-linear-to-b from-violet-50 to-pink-100 md:translate-y-[18px] shadow-violet-600/20 shadow-xl hover:scale-105 transition-all ease-in-out">
-						<Image src="/make-money.png" alt="make-money icon" width={50} height={50} />
-						<div className="flex-col gap-4 items-center justify-between py-3.5 rounded-lg px-2 ">
-							<h2 className="font-medium py-2">Make More Money</h2>
-							<p>We help free up your time so you can focus on what you do best - running your business.</p>
+
+					{service.map((service) => (
+						<div key={service.id} className="flex sm:flex-col items-center justify-center text-center h-[23%] w-[90%] md:w-[23%] lg:w-[20%] rounded-lg  min-h-[28rem] py-4 px-2 m-2 sm:m-3 lg:m-4 bg-linear-to-b from-violet-50 to-pink-100 md:translate-y-[18px] shadow-violet-600/20 shadow-xl hover:scale-105 transition-all ease-in-out">
+							<Image src={service.imgUrl} alt="make-money icon" width={50} height={50} />
+							<div className="flex-col gap-4 items-center justify-between py-3.5 rounded-lg px-2 ">
+								<h2 className="font-medium py-2">{service.title}</h2>
+								<p>{service.description}</p>
+							</div>
 						</div>
-					</div>
-					<div className="flex sm:flex-col items-center justify-center text-center h-[23%] w-[90%] md:w-[23%] lg:w-[20%] rounded-lg  min-h-[28rem] py-4 px-2 m-2 sm:m-3 lg:m-4 bg-linear-to-b from-violet-50 to-pink-100 md:translate-y-[-18px] shadow-violet-600/20 shadow-xl hover:scale-105 transition-all ease-in-out">
-						<Image src="/like.png" alt="Like icon" width={50} height={50} />
-						<div className="flex-col gap-4 items-center justify-between py-3.5 px-2 ">
-							<h2 className="font-medium py-2">Turn Likes into Clients</h2>
-							<p>We help you convert your online audience into loyal customers and boost your bottom line.</p>
-						</div>
-					</div>
-					<div className="flex sm:flex-col items-center justify-center text-center h-[23%] w-[90%] md:w-[23%] lg:w-[20%] rounded-lg  min-h-[28rem] py-4 px-2 m-2 sm:m-3 lg:m-4 bg-linear-to-b from-violet-50 to-pink-100 md:translate-y-[18px] shadow-violet-600/20 shadow-xl hover:scale-105 transition-all ease-in-out">
-						<Image src="/social.png" alt="Social icon" width={50} height={50} />
-						<div className="flex-col gap-4 items-center justify-between py-3.5 px-2 ">
-							<h2 className="font-medium py-2">Level Your Social Media Skills</h2>
-							<p>Take advantage of our free ebooks, training programs and workshops to stay ahead of the curve</p>
-						</div>
-					</div>
-					<div className="flex sm:flex-col items-center justify-center text-center h-[23%] w-[90%] md:w-[23%] lg:w-[20%] rounded-lg  min-h-[28rem] py-4 px-2 m-2 sm:m-3 lg:m-4 bg-linear-to-b from-violet-50 to-pink-100 md:translate-y-[-18px] shadow-violet-600/20 shadow-xl hover:scale-105 transition-all ease-in-out">
-						<Image src="/grow.png" alt="Grow icon" width={50} height={50} />
-						<div className="flex-col gap-4 items-center justify-between py-3.5 px-2 ">
-							<h2 className="font-medium py-2">Grow Your Business On Social Media</h2>
-							<p>We help you identify your ideal audience, craft targeted content and ensure you&apos;re using the right platforms to achieve your goals.</p>
-						</div>
-					</div>
+					))}
+
+
+
 				</div>
 
 		</div>
