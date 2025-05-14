41709c8eb294b79a9d91d02d73014c1f4314fd15
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-14 14:18:12 +0300

    layout fixes

--- components/ForIndividuals.tsx
+++ components/ForIndividuals.tsx
@@ -31,7 +31,7 @@ const ForIndividuals = () => {
 	]
 
 	return (
-		<div className="flex justify-between h-full items-center rounded-lg  gap-8 m-4 my-24 px-3 py-4">
+		<div className="flex md:w-[90%] flex-col md:flex-row justify-between min-h-fit items-center rounded-lg  gap-8 m-4 my-24 px-3 py-4">
 			{individual.map((item) => (
 				<div key={item.id} className="flex flex-col items-center justify-between h-100 md:h-full py-4 px-2 shadow-md shadow-violet-300 gap-y-4 bg-linear-to-b from-violet-50 to-pink-100">
 					<Image src={item.imgurl} alt={item.alt} width={500} height={500} className="rounded-md flex" />
