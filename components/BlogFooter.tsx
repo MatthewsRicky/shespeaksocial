563b6787f6c03f817de27ca7e8026444f6d6884d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-16 20:56:31 +0300

    Blog footer init

---
+++ components/BlogFooter.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const BlogFooter = () => {
+	return (
+		<div className="w-full h-48 bg-slate-700 flex items-center justify-center mx-auto">BlogFooter</div>
+	)
+}
+export default BlogFooter
