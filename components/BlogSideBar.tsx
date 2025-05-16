4427494562500451f446e84e6b87af47e23c86dc
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-16 20:59:28 +0300

    layout and positioning

--- components/BlogSideBar.tsx
+++ components/BlogSideBar.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const BlogSideBar = () => {
 	return (
-		<div className="max-h-fit flex flex-col items-center justify-center mx-auto bg-slate-700  w-full text-white">
+		<div className="h-fit flex flex-col gap items-center justify-center mx-auto  mt-32 ">
 				{posts.map((post) => (
-					<section key={post.id} className="flex flex-col items-center justify-center W-full">
-						<Image src={post.imageUrl} alt={post.imageAlt} width={400} height={400} />
+					<section key={post.id} className="flex flex-col items-center justify-cente rw-full text-white bg-slate-700">
+						<Image src={post.imageUrl} alt={post.imageAlt} width={400} height={400} className="flex" />
 					</section>
 				))}
 		</div>
