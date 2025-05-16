63481bb0c161088a8443b53114bf9bd84a359b00
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-16 20:58:26 +0300

    initial styling and layout work

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,25 +6,26 @@ import Image from "next/image";
 
 const Blog = () => {
 	return (
-		<main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full items-center justify-center mx-auto overflow-x-hidden lg:mt-48">
-			<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-6">
-				{posts.map((post) => (
-					<div key={post.id} className="relative flex flex-col items-center justify-center w-full gap-2 h-96">
-						<Image src={post.imageUrl} alt={post.imageAlt} width={200} height={200} className="flex"/>
-						<span>{post.imageTitle}</span>
-						<h1>{post.title}</h1>
-						<span>
+		<main className="relative flex">
+      <section className="lg:w-[67%] grid md:grid-cols-2 lg:grid-cols-3 gap-8 items-center justify-center mt-48 lg:mt-32">
+	      {posts.map((post) => (
+		      <div key={post.id} className="relative flex flex-col items-center justify-center h-full  bg-linear-to-b from-pink-50 to-pink-100 sm:mx-4 w-[95%] mx-auto rounded-md gap-4">
+			      <Image src={post.imageUrl} alt={post.imageAlt} width={500} height={500} className="flex w-[70%] h-[70%]"/>
+			      <span>{post.imageTitle}</span>
+			      <h1>{post.title}</h1>
+			      <span>
 							<h2>{post.author}</h2>
 							<h3>{post.date}</h3>
 						</span>
-						<p>{post.article}</p>
-					</div>
+			      <p>{post.article}</p>
+		      </div>
 
-				))}
-				<div className="hidden w-full h-fit lg:flex">
+	      ))}
+      </section>
+				<div className="hidden lg:fixed right-0 w-[30%] h-fit lg:flex">
 					<BlogSideBar />
 				</div>
-			</section>
+
 
 
 		</main>
