a55c07102575fb01a19511a1b33e39ce8489ff25
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-17 13:37:43 +0300

    fitting it to the rest of the blog layout and positioning

--- components/BlogSideBar.tsx
+++ components/BlogSideBar.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {posts} from "@/utils/constants";
-import Image from "next/image";
+
 
 const BlogSideBar = () => {
 	return (
-		<div className="h-fit flex flex-col gap items-center justify-center mx-auto  mt-32 ">
-				{posts.map((post) => (
-					<section key={post.id} className="flex flex-col items-center justify-cente rw-full text-white bg-slate-700">
-						<Image src={post.imageUrl} alt={post.imageAlt} width={400} height={400} className="flex" />
-					</section>
-				))}
+		<div className="h-fit flex scroll-auto flex-col gap items-center justify-center mx-auto  mt-32 ">
+			<div>Dont Miss</div>
+			<div>Stay in Touch </div>
+			<div>Our Picks</div>
+			<div>Hot</div>
+			<div>Subscribe</div>
 		</div>
 	)
 }
