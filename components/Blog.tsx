429cd219c6c4ad4f29746557f4dfb5a19b831254
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-18 17:50:17 +0300

    container styling and color blocking

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 
 const Blog = () => {
 	return (
-		<main className="relative flex items-center gap-8 justify-center">
+		<main className="relative flex items-center gap-8 justify-center lg:w-[95%]">
       <section className="md:w-[70%] lg:w-[65%] grid md:grid-cols-2 gap-8 items-center justify-center mt-48 lg:my-32">
 	      {posts.map((post) => (
-		      <div key={post.id} className="relative bg-gradient-to-b from-pink-50 to-violet-100 shadow-md shadow-violet-300 flex flex-col items-center justify-between h-full w-[95%] text-start mx-auto rounded-md gap-4">
+		      <div key={post.id} className="relative bg-white shadow-md shadow-violet-300 flex flex-col items-center justify-between h-full w-[95%] text-start mx-auto rounded-md gap-4">
 			      <div className="relative flex items-center justify-start text-start w-full h-64 rounded-lg">
 				      <Image src={post.imageUrl} alt={post.imageAlt} width={500} height={500} className="absolute flex w-full justify-start h-full rounded-md" />
 				      <span className="absolute left-0 bottom-0 text-slate-100 bg-slate-900 hover:bg-deep-pink/70 px-2 py-1 rounded-md">{post.imageTitle}</span>
