df55aa58bc41a099114a34a1518605dcc6c48b94
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-19 12:45:40 +0300

    cHANGING tWITTER TO X

--- components/StayInTouch.tsx
+++ components/StayInTouch.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BsFacebook, BsInstagram, BsPinterest, BsTwitter, BsVimeo, BsYoutube} from "react-icons/bs";
+import {BsFacebook, BsInstagram, BsPinterest, BsVimeo, BsX, BsYoutube} from "react-icons/bs";
 
 const StayInTouch = () => {
 	return (
@@ -7,7 +7,7 @@ const StayInTouch = () => {
 			<h1 className="text-slate-900 mb-8 underline underline-offset-8 w-full font-bold leading-none">Stay In Touch</h1>
 			<div className="grid grid-cols-1 grid-rows-6 w-full gap-3">
 				<span className="flex w-full items-center justify-start gap-3 mx-auto bg-facebook px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-facebook/50"><BsFacebook /> <h2>Facebook</h2></span>
-				<span className="flex w-full items-center justify-start gap-3 bg-black mx-auto px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-black/50"><BsTwitter /> <h2>Twitter</h2></span>
+				<span className="flex w-full items-center justify-start gap-3 bg-black mx-auto px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-black/50"><BsX /> <h2>X</h2></span>
 				<span className="flex w-full items-center justify-start gap-3 mx-auto bg-pinterest px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-pinterest/50"><BsPinterest /> <h2>Pinterest</h2></span>
 				<span className="flex w-full items-center justify-start gap-3 mx-auto bg-instagram px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-instagram/50"><BsInstagram /> <h2>Instagram</h2></span>
 				<span className="flex w-full items-center justify-start gap-3 mx-auto bg-youtube px-3 py-2 font-medium text-white rounded-md shadow-lg shadow-youtube/50"><BsYoutube /> <h2>YouTube</h2></span>
