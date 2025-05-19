e33db3609b46e5e518acc62ec0a355964fa931b8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-19 10:31:22 +0300

    Factoring out the cards into solo components then importing them for a cleaner sidebar

--- components/BlogSideBar.tsx
+++ components/BlogSideBar.tsx
@@ -1,34 +1,21 @@
 import React from 'react'
-import Image from "next/image";
+
+import StayInTouch from "@/components/StayInTouch";
+import OurPick from "@/components/OurPick";
+import Hot from "@/components/Hot";
+import Subscribe from "@/components/Subscribe";
+import DontMiss from "@/components/DontMiss";
 
 
 const BlogSideBar = () => {
 	return (
 		<div className="flex scroll-auto flex-col gap items-center justify-between h-screen mt-32 px-2 mx-2">
 			<div className="flex  flex-col h-full justify-start gap-6">
-				<div className="flex flex-col items-center justify-between gap-4 h-fit">
-					<h1 className="text-slate-900 mb-8 underline underline-offset-8 w-full font-bold leading-none">
-						Don&apos;t Miss
-					</h1>
-					<div className="relative flex items-center justify-center w-full">
-						<Image src="/career.jpg" alt="Carrers" height={500} width={500} className="flex items-center justify-center w-full h-full" />
-						<span className="absolute left-1 bottom-0 rounded text-white  bg-slate-800/80 px-2 py-1 hover:bg-deep-pink/70 z-10 flex">
-						Careers
-					</span>
-					</div>
-					<h1 className="flex items-center justify-start w-full">How ToMake Your X(Twitter) account Private.</h1>
-					<span className="flex items-center justify-start w-full">
-						<h2>She Speaks Social,</h2>
-						<h3>November 14th, 2024</h3>
-					</span>
-					<p className="text-gray-600 leading-none">
-						Ever feel like you want a bit more control over who sees your tweets? Whether…
-					</p>
-				</div>
-				<div>Stay in Touch </div>
-				<div>Our Picks</div>
-				<div>Hot</div>
-				<div>Subscribe</div>
+				<div><DontMiss /></div>
+				<div><StayInTouch /> </div>
+				<div><OurPick /></div>
+				<div><Hot /></div>
+				<div><Subscribe /></div>
 			</div>
 
 		</div>
