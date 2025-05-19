c04c03106fa892cf7abe9d74e9283cc17731c989
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-19 14:00:02 +0300

    Homogenizing and color blocking

--- components/BlogFooter.tsx
+++ components/BlogFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogFooter = () => {
 	return (
-		<div className="w-full h-48 bg-slate-700 flex items-center justify-center mx-auto">BlogFooter</div>
+		<div className="w-full h-48 bg-slate-900 grid grid-cols-3 grid-row-4 items-center justify-center mx-auto">BlogFooter</div>
 	)
 }
 export default BlogFooter
