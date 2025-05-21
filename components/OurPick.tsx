38c8cfaf3cb468807814217603a8f3c507662127
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-21 22:51:34 +0300

    Shadow for the inner image

--- components/OurPick.tsx
+++ components/OurPick.tsx
@@ -7,7 +7,7 @@ const OurPick = () => {
 			<h1 className="text-slate-900 mb-8 underline underline-offset-8 w-full font-bold leading-none">Our Pick</h1>
 			<div className="bg-white flex flex-col h-full items-start justify-start p-2 rounded-lg shadow-lg shadow-slate-900">
 				<div className="grid grid-cols-2 gap-2 w-full">
-					<Image src="/our-picks.jpg" alt="Our Picks" height={500} width={500} className="rounded-md flex w-full h-[70%]"/>
+					<Image src="/our-picks.jpg" alt="Our Picks" height={500} width={500} className="rounded-md flex w-full h-[70%] shadow-sm shadow-slate-900/60"/>
 					<section className="flex flex-col w-full justify-center gap-2">
 						<h1 className="font-medium leading-5 text-md">
 							A Beginner&apos;s Guide Social Media Marketing for Startups in Nairobi
