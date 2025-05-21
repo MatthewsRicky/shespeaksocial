8e43bab402d18be41c556adaf24d71272df36a3e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-21 19:20:18 +0300

    more layout design

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,18 +6,18 @@ import Image from "next/image";
 
 const Blog = () => {
 	return (
-		<main className="relative flex items-start gap-8 justify-center lg:w-[95%]">
-      <section className="md:w-[70%] lg:w-[65%] grid md:grid-cols-2 gap-8 items-center justify-center mt-48 lg:my-32">
+		<main className="relative flex items-start gap-6 justify-center lg:w-[85%]">
+      <section className="md:w-[70%] lg:w-[65%] grid md:grid-cols-2 gap-4 items-center justify-center mt-48 lg:my-32">
 	      {posts.map((post) => (
-		      <div key={post.id} className="relative bg-white shadow-md shadow-slate-800 flex flex-col items-center justify-between h-[85%] w-[95%] text-start mx-auto rounded-md gap-4">
+		      <div key={post.id} className="relative bg-white shadow-md shadow-slate-800 flex flex-col items-center justify-between h-[85%] w-[90%] text-start mx-auto rounded-md gap-4">
 			      <div className="relative flex items-center justify-start text-start w-full h-64 rounded-lg">
 				      <Image src={post.imageUrl} alt={post.imageAlt} width={400} height={300} className="absolute flex w-full justify-start h-full rounded-md" />
-				      <span className="absolute left-0 bottom-0 text-slate-100 bg-slate-900 hover:bg-deep-pink/70 px-2 py-1 rounded-md">{post.imageTitle}</span>
+				      <span className="absolute left-0 bottom-0 text-slate-100 bg-slate-900 hover:bg-razzmataz text-sm px-2 py-1 rounded-sm">{post.imageTitle}</span>
 			      </div>
-			      <div className="flex flex-col items-start justify-start gap-4 p-1 ">
-				      <h1 className="text-xl font-bold ">{post.title}</h1>
+			      <div className="flex flex-col items-start justify-start gap-4 p-1 text-sm">
+				      <h1 className="text-lg font-bold ">{post.title}</h1>
 				      <span className="flex gap-4 items-center ml-o -w-full">
-							<h2 className="font-semibold">{post.author},</h2>
+							<h2 className="font-semibold text-xs">{post.author},</h2>
 							<h3>{post.date}</h3>
 						</span>
 				      <p>{post.article}</p>
