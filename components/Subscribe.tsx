d3e1c6ff14a558f8144a8350199712b9abe5654c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-21 22:51:56 +0300

    Subscribe form blocking

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Form from "next/form";
 
 const Subscribe = () => {
 	return (
-		<div>Subscribe</div>
+		<div>
+			<Form action="">
+				<input name="email" type="email" placeholder="Email" />
+				<button type="submit">Subscribe</button>
+			</Form>
+
+		</div>
 	)
 }
 export default Subscribe
