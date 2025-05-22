a8e3b93733c997ddb86ac3ba3361cde266a6cd8e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-22 12:38:09 +0300

    full color blocking and layout

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -4,10 +4,17 @@ import Form from "next/form";
 const Subscribe = () => {
 	return (
 		<div>
+			<h1 className="text-xl text-center font-bold">Subscribe To Updates</h1>
 			<Form action="">
-				<input name="email" type="email" placeholder="Email" />
-				<button type="submit">Subscribe</button>
+				<input name="email" type="email" placeholder="Email" className="flex w-full bg-white px-4 py-3 rounded-md border border-gray-200 shadow-md shadow-slate-900/30 my-6" />
+				<button type="submit" className="flex text-center text-lg font-bold w-full bg-youtube text-white items-center justify-center py-3 rounded-lg shadow-md shadow-slate-900/30">Subscribe</button>
 			</Form>
+			<div className="flex items-center justify-start my-10 gap-2">
+				<input type="checkbox" className="p-4"  />
+				<label htmlFor="select">
+					<p className="text-xs font-medium text-slate-700">By signing up you agree to our terms and our Privacy Policy agreement.</p>
+				</label>
+			</div>
 
 		</div>
 	)
