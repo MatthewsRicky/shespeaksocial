8d043c94796edbe2049a8c5616f6105a82b20d03
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-25 21:46:33 +0300

    Using the data from the api call to currents and blocking out simple component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+// components/NewsList.tsx
+"use client"
+import { useEffect, useState } from 'react';
 
-const Posts = () => {
+type NewsArticle = {
+	id: string;
+	title: string;
+	description: string;
+	url: string;
+	image: string;
+	published: string;
+};
+
+export default function Post() {
+	const [articles, setArticles] = useState<NewsArticle[]>([]);
+
+	useEffect(() => {
+		fetch('/api/news?category=technology')
+			.then(res => res.json())
+			.then(data => setArticles(data.news || []));
+	}, []);
+
 	return (
-		<div>Posts</div>
-	)
+		<div>
+			<h2 className="text-xl font-bold">Latest News</h2>
+			<ul>
+				{articles.map(article => (
+					<li key={article.id} className="mb-4">
+						<a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
+							{article.title}
+						</a>
+						<p>{article.description}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
-export default Posts
