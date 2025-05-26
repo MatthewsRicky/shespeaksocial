4e6c70acea9be261cca940ad6f20783d2abdb27f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-26 21:35:32 +0300

    Building out the posts client component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,6 +1,8 @@
 // components/NewsList.tsx
 "use client"
 import { useEffect, useState } from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 type NewsArticle = {
 	id: string;
@@ -9,15 +11,20 @@ type NewsArticle = {
 	url: string;
 	image: string;
 	published: string;
+	language: string;
+	category: string;
+	author?: string;
 };
 
 export default function Post() {
 	const [articles, setArticles] = useState<NewsArticle[]>([]);
 
 	useEffect(() => {
-		fetch('/api/news?category=technology')
+		fetch('https://api.currentsapi.services/v1/latest-news')
 			.then(res => res.json())
 			.then(data => setArticles(data.news || []));
+
+		console.log(articles);
 	}, []);
 
 	return (
@@ -25,10 +32,13 @@ export default function Post() {
 			<h2 className="text-xl font-bold">Latest News</h2>
 			<ul>
 				{articles.map(article => (
-					<li key={article.id} className="mb-4">
-						<a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
+
+					<li key={article.id} className=" mb-4 flex bg-cararra justify-center items-center gap-6">
+						<Link href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
 							{article.title}
-						</a>
+						</Link>
+						<Image src={article.image} alt="pic" width={200} height={200} />
+						<h2>{article.author}</h2>
 						<p>{article.description}</p>
 					</li>
 				))}
