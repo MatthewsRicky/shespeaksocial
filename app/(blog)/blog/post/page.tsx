dcc9bf5cffe7833784a47553255c895d2a4d3c1b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-27 11:58:59 +0300

    fixing broken fetch call

--- app/(blog)/blog/post/page.tsx
+++ app/(blog)/blog/post/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
-const Page = () => {
+
+export default async function Page() {
+
+
+	const data = await fetch('https://api.currentsapi.services/v1/latest-news?category=${category}&apiKey=${CURRENTS_API_KEY}')
+	const post = await data.json()
+
+	console.log(post)
 	return (
-		<div>Page</div>
+		<div className="flex h-screen">
+			{post.map((post) => (
+				<div key={post.id}>
+					<h1>{post.title}</h1>
+				</div>
+			))}
+		</div>
 	)
 }
-export default Page
