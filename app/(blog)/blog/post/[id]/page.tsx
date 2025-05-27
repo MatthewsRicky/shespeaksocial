82ef4229d1a94683d6228904413bc8d84691f22b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-27 23:25:21 +0300

    Figuring out dynamic rendering

--- app/(blog)/blog/post/[id]/page.tsx
+++ app/(blog)/blog/post/[id]/page.tsx
@@ -1,14 +1,13 @@
-import Posts from '@/components/Posts'
-import React, {Suspense} from 'react'
+import React from 'react'
 
-const Page = () => {
+async function Page() {
+	const response = await fetch(`${process.env.CURRENTS_API_ENDPOINT}?apiKey=${process.env.CURRENTS_API_KEY}/1`)
+	const post = await response.json();
 	return (
-		<main className="h-screen">
-			<Suspense fallback={<div>Loading...</div>}>
-				<Posts />
-			</Suspense>
-
+		<main className="flex h-screen ">
+			<h1 key={post.id} className="flex items-center justify-center mx-auto text-xl font-bold mt-48">{post.title}</h1>
 		</main>
 	)
 }
+
 export default Page
