6eabdcb0d7f5e2c6dc1e5f22536427d0ea3dc16f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-27 23:32:17 +0300

    Fixing original bog

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Blog from "@/components/Blog";
-
 const About = () => {
 	return (
 		<div className="flex items-center justify-center w-full lg:w-95vw xl:w-90vw mx-auto">
