da5734cb3542d0be651d80d78cb67b39758c2902
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-05-28 09:48:47 +0300

    Creating links from the rendered posts blog cards to the dynamic single id post page

--- app/(blog)/blog/post/page.tsx
+++ app/(blog)/blog/post/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Posts() {
 
@@ -11,9 +13,13 @@ export default async function Posts() {
 	return (
 		<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 m-2 grid-rows-10 w-[95%] h-full ">
 			{Array.isArray(news) && news.map((post ) => (
-				<div key={post.id} className="flex items-center bg-serenade justify-center text-black gap-4 flex-col p-2">
-					<h1 className="text-md font-bold leading-6">{post.title}</h1>
-					<p className="text-sm font-regular leading-6">{post.description}</p>
+				<div key={post.id}>
+					<Link href={`/blog/post/${post.id}`}  className="flex items-center bg-serenade justify-center text-black gap-4 flex-col p-2">
+						<Image src={"/post.image"} alt="picture" width={500} height={500} />
+						<h1 className="text-md font-bold leading-6">{post.title}</h1>
+						<p className="text-sm font-regular leading-6">{post.description}</p>
+					</Link>
+
 				</div>
 			))}
 		</div>
